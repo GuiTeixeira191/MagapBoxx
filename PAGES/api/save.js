3b7d1d08b4c9e5fccf5f844189190c4678886c10
GuiTeixeira191 <guilhermeart2020@outlook.com>
2022-08-31 18:29:58 -0300

    fix base64

--- PAGES/api/save.js
+++ PAGES/api/save.js
@@ -1,4 +1,4 @@
-
+import { fromBase64 } from '../../utils/base64'
 import { GoogleSpreadsheet } from 'google-spreadsheet'
 import moment from 'moment'
 
@@ -12,7 +12,7 @@ export default async (req, res) => {
     try {
         await doc.useServiceAccountAuth({
             client_email: process.env.SHEET_CLIENT_EMAIL,
-            private_key: process.env.SHEET_PRIVATE_KEY
+            private_key: fromBase64(process.env.SHEET_PRIVATE_KEY)
         })
         await doc.loadInfo()   
         const sheet = doc.sheetsByIndex[1]
